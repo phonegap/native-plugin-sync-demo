6293d6e1b06ae993f2ee28cbd6ad3eba7bce1828
Shazron Abdullah <shazron@apache.org>
2016-01-27 12:22:07 -0700

    Add back in the echo-plugin mistake.

--- echo-plugin/echo-plugin/EchoPlugin.m
+++ echo-plugin/echo-plugin/EchoPlugin.m
@@ -5,10 +5,9 @@
 - (void) echo:(CDVInvokedUrlCommand*)command
 {
     NSString* message = [command argumentAtIndex:0];
-//    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:
-//                                     [message stringByAppendingString:message]];
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:
-                                     message];
+    message = [message stringByAppendingString:message]; // uh-oh?
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: message];
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
